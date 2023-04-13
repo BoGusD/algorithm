1c309ada17cb0b0cac1a57c03f6646291e63e67c
bogus <qazxwd44@hanmail.net>
2023-04-13 19:08:56 +0900

    삽입, 버블정렬

---
+++ inflern/section 7(이분 검색)/특별 정렬(구글 인터뷰).js
@@ -0,0 +1,29 @@
+// 버블 정렬
+solution = (arr) => {
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+};
+let a = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(a));
+
+// 스택 사용시
+function solution(arr) {
+  let answer = [];
+  for (let x of arr) {
+    if (x < 0) answer.push(x);
+  }
+  for (let x of arr) {
+    if (x > 0) answer.push(x);
+  }
+  return answer;
+}
+
+let arr = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr));
