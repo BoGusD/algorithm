20198b908aa408171aa9442ad69d3b2729298e85
bogus <qazxwd44@hanmail.net>
2023-06-15 22:46:39 +0900

    알고리즘 하루 일당

--- programmers/level 0/문자열 겹쳐쓰기.js
+++ programmers/level 0/문자열 겹쳐쓰기.js
@@ -1,5 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181943
 
+// splice(시작 인덱스, 대체할 인덱스 길이, 인입할 대상)
+
 function solution(my_string, overwrite_string, s) {
   const myStr = [...my_string];
   myStr.splice(s, overwrite_string.length, overwrite_string);
