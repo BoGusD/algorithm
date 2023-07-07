768c9e2c27d0f69a08451d788aece8c6b3250e33
unknown <qazxwd44@hanmail.net>
2023-07-08 03:50:23 +0900

    마지막 알고리즘

---
+++ programmers/level 0/접미사인지 확인하기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181908
+//endsWith << 문자열이 포함되어있는지 확인할 때 사용하는 내장 함수
+solution = (my_string, is_suffix) => {
+  return my_string.endsWith(is_suffix) ? 1 : 0;
+};
